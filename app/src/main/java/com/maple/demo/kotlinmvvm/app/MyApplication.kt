6d3a5acbdf930fefce8165fb658d7635049b0a8a
gaoguanqi <307590625@qq.com>
2019-04-02 10:20:16 +0800

    init

--- app/src/main/java/com/maple/demo/kotlinmvvm/app/MyApplication.kt
+++ app/src/main/java/com/maple/demo/kotlinmvvm/app/MyApplication.kt
@@ -2,12 +2,7 @@ package com.maple.demo.kotlinmvvm.app
 
 import android.app.Application
 import com.blankj.utilcode.util.Utils
-import com.maple.demo.kotlinmvvm.BuildConfig
 import es.dmoral.toasty.Toasty
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.Logger
-import com.orhanobut.logger.Logger.addLogAdapter
-
 
 
 /**
@@ -27,11 +22,6 @@ class MyApplication : Application(){
         instance = this
         Utils.init(this)
         Toasty.Config.getInstance().setTextSize(14).apply()
-        Logger.addLogAdapter(object :AndroidLogAdapter(){
-            override fun isLoggable(priority: Int, tag: String?): Boolean {
-                return BuildConfig.DEBUG
-            }
-        })
 
     }
 }
--- app/src/main/java/com/maple/demo/kotlinmvvm/model/service/ApiService.kt
+++ app/src/main/java/com/maple/demo/kotlinmvvm/model/service/ApiService.kt
@@ -2,14 +2,11 @@ package com.maple.demo.kotlinmvvm.model.service
 
 /**
  * author: gaogq
- * time: 2019/3/29 11:07
+ * time: 2019/4/2 9:28
  * description:
  */
 interface ApiService {
-
     companion object {
         val BASE_URL:String ="https://api.douban.com"
     }
-
-
 }
