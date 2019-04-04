e5d5842e99fb6758411cb74fb91c00e97015d281
gaoguanqi <307590625@qq.com>
2019-04-04 17:37:27 +0800

    init

--- app/src/main/java/com/maple/demo/kotlinmvvm/app/base/BaseViewActivity.kt
+++ app/src/main/java/com/maple/demo/kotlinmvvm/app/base/BaseViewActivity.kt
@@ -1,6 +1,11 @@
 package com.maple.demo.kotlinmvvm.app.base
 
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import com.maple.demo.kotlinmvvm.R
+import com.maple.demo.kotlinmvvm.app.manager.state.MultipleStatusView
+import kotlinx.android.synthetic.main.layout_base.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 /**
  * author: gaogq
@@ -9,7 +14,24 @@ import android.os.Bundle
  */
 abstract class BaseViewActivity : BaseActivity(){
     override fun onContentView() {
-       setContentView(layoutResID())
+       setContentView(R.layout.layout_base)
+        initToolbar(toolbar)
+        initMultipleStatusView(multiple_status_view)
+    }
+
+    open fun initMultipleStatusView(multipleStatusView: MultipleStatusView?){
+        if(multipleStatusView == null){
+            return
+        }
+        multipleStatusView.setSuccessView(layoutResID())
+    }
+
+    open fun initToolbar(toolbar: Toolbar?){
+        if(toolbar == null){
+            return
+        }
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
     }
 
     override fun initCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/maple/demo/kotlinmvvm/test/TestActivity.kt
+++ app/src/main/java/com/maple/demo/kotlinmvvm/test/TestActivity.kt
@@ -1,18 +1,9 @@
 package com.maple.demo.kotlinmvvm.test
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.maple.demo.kotlinmvvm.R
-import kotlinx.android.synthetic.main.layout_toolbar.*
+import com.maple.demo.kotlinmvvm.app.base.BaseViewActivity
 
-class TestActivity : AppCompatActivity() {
+class TestActivity : BaseViewActivity() {
+    override fun layoutResID(): Int = R.layout.activity_test
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
-
-
-        setSupportActionBar(toolbar)
-        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
-    }
 }
